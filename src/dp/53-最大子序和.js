6ba7d6126a25f47a05fe63419b00b3d0334620f2
JowayYoung <young.joway@aliyun.com>
2020-11-23 11:29:46 +0800

    53-最大子序和

---
+++ src/dp/53-最大子序和.js
@@ -0,0 +1,20 @@
+/**
+ * @name 53.最大子序和
+ * @param {number[]} [nums=[]]
+ * @return {number}
+ * @method max(dp[0],dp[1],...,d[i-1],dp[i])
+ * @summary 状态转移方程
+ */
+function MaxSubArray(nums = []) {
+	if (!nums.length) return 0;
+	const dp = new Array(nums.length);
+	let res = nums[0];
+	dp[0] = nums[0];
+	for (let i = 1; i < nums.length; i++) {
+		dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+		res = Math.max(dp[i], res);
+	}
+	return res;
+}
+
+console.log(MaxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
--- src/dp/70-爬楼梯.js
+++ src/dp/70-爬楼梯.js
@@ -2,18 +2,18 @@
  * @name 70.爬楼梯
  * @param {number} [n=0]
  * @return {number}
- * @method 到达第n阶的方法总数是到达第(n-1)阶和到达第(n-2)阶的方法总数之和
- * @summary 斐波那契数列
+ * @method dp[n]=dp[n-1]+dp[n-2]
+ * @summary 状态转移方程
  */
 function ClimbStairs(n = 0) {
-	const res = new Array(n + 1);
-	res[0] = 0;
-	res[1] = 1;
-	res[2] = 2;
+	const dp = new Array(n + 1);
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
 	for (let i = 3; i <= n; i++) {
-		res[i] = res[i - 1] + res[i - 2];
+		dp[i] = dp[i - 1] + dp[i - 2];
 	}
-	return res[n];
+	return dp[n];
 }
 
 console.log(ClimbStairs(5));
